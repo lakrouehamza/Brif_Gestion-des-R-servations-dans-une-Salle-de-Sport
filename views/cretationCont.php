f05bf751da6157c41584e1e5390a9a31114cab58
lakrouehamza <lakrounehamza10@gmail.com>
2024-12-13 09:49:08 +0100

    ajoute structore de insert reservation

--- views/cretationCont.php
+++ views/cretationCont.php
@@ -62,7 +62,7 @@ if(mysqli_connect_errno()){
             </label>
             <input
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="phone" type="tel" placeholder="Enter your phone number">
+                id="phone" type="tel" name="phone" placeholder="Enter your phone number">
         </div>
         <div class="mb-4">
             <label class="block text-gray-700 font-bold mb-2" for="activite">
@@ -84,21 +84,14 @@ if(mysqli_connect_errno()){
             </label>
             <input
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="date" type="date" placeholder="Select a date">
+                id="date" type="date" name="dateR" placeholder="Select a date">
         </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 font-bold mb-2" for="message">
-                Message
-            </label>
-            <textarea
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="message" rows="4" placeholder="Enter any additional information"></textarea>
-        </div>
+
         <div class="flex items-center justify-center mb-4">
             <button
                 class="bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-800 focus:outline-none focus:shadow-outline"
-                type="submit">
-                Book Appointment
+                type="submit" name="conservation">
+                conservation
             </button>
         </div>
 
@@ -109,6 +102,52 @@ if(mysqli_connect_errno()){
     <img src="https://images.unsplash.com/photo-1536147116438-62679a5e01f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="Leafs" class="w-full h-48 object-cover sm:h-screen sm:w-4/12">
   </div>
     </section>
+     <?php
+        //2- insert from database
+        
+          // button click  
+if (isset($_POST['conservation'])) { 
+
+ $nom=$_POST['nom'];
+  $prenom=$_POST['prenom'];
+  $email =$_POST['email'];
+  $phone =$_POST['phone'];
+  $dateR =$_POST['dateR'];
+   $query="insert into  client(nom,prenom,email,telephone) values('$nom','$prenom','$email','$phone');";
+        $result=  mysqli_query($connect, $query);
+    
+        if( $result){
+            echo '</br>Data is inerted';
+        } 
+ else {
+die("Database query failed. " . mysqli_error($connection));
+ }
+}
+       
+?>
+             <?php
+        //2- insert from database
+        
+          // button click  
+if (isset($_POST['conservation'])) { 
+
+ $nom=$_POST['nom'];
+  $prenom=$_POST['prenom'];
+  $email =$_POST['email'];
+  $phone =$_POST['phone'];
+  $dateR =$_POST['dateR'];
+   $query="insert into  reservation(id_client,id_activite,date_resevation,statut)
+           values(1,1,'01/01/2025','active');";
+        $result=  mysqli_query($connect, $query);
+    
+        if( $result){
+            echo '</br>Data is inerted';
+        } 
+ else {
+die("Database query failed. " . mysqli_error($connection));
+ }
+}
+        ?>
 </body>
 </html>
 <?php
